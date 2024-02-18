d76ba9330932b9d7d3945a73d113c5a44cc04c92
Luca <luca.ambrosonee@gmail.com>
2024-02-18 22:33:21 +0100

    Added navbar animation and app tabs

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+    School Timetable - JavaScript Functions
+    What're you doing here?
+*/
+
+// ---------------  NAVBAR  ---------------
+
+let currentTab = 1;
+const navbar = document.getElementById("navbar");
+const navbarBtn = document.querySelectorAll(".navbarBtn");
+const navbarIcon = document.querySelectorAll(".navbarIcon");
+const navbarLabel = document.querySelectorAll(".navbarLabel");
+const appTabs = document.querySelectorAll(".appTabs");
+appTabs[0].classList.add("appTabsShown");
+
+function navbarAction(tab) {
+  if (currentTab !== tab) {
+    currentTab = tab;
+    navbarIcon.forEach(item => item.classList.remove("navbarIconActive"));
+    navbarLabel.forEach(item => item.classList.remove("navbarLabelActive"));
+    navbarIcon[tab].classList.add("navbarIconActive");
+    navbarLabel[tab].classList.add("navbarLabelActive");
+    appTabs.forEach(appTab => appTab.classList.remove("appTabsShown"));
+    appTabs.forEach(appTab => appTab.classList.add("appTabsHidden"));
+    appTabs[tab].classList.remove("appTabsHidden");
+    appTabs[tab].classList.add("appTabsShown");
+  }
+}
